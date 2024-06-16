ffda9082f543b5900a5c81fff62fb28c35eccc7f
Prixons <163110375+Prixons@users.noreply.github.com>
2024-06-16 09:53:25 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,4 @@
-const sortByDescendingFriendCount = users => {
-  return users.sort((a, b) => b.friends.length - a.friends.length);
-};
+const sortByDescendingFriendCount = users => users.toSorted((a, b) => b.friends.length - a.friends.length);
 
 console.log(
   sortByDescendingFriendCount([
@@ -41,4 +39,3 @@ console.log(
     }
   ])
 );
-
